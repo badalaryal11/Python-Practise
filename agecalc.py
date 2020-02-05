f2d2440e6580dc11ec21bf7909ca3a3b2f98859a
Badal Aryal <35680989+badalaryal11@users.noreply.github.com>
2020-02-05 23:02:21 +0545

    Add files via upload

---
+++ agecalc.py
@@ -0,0 +1,22 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #simple program to your age....
+>>> print('Answer the questions to know how long you have lived in this life.... ')
+Answer the questions to know how long you have lived in this life.... 
+>>> name=input('Name:')
+Name:Badal
+>>> print('what is your age?',(name),'?')
+what is your age? Badal ?
+>>> 22
+22
+>>> age=int(input('age:'))
+age:23
+>>> days= age*365
+>>> minutes=age*525948
+>>> seconds=age*31556926
+>>> print(name,'has been alive for',days,'days',minutes,'minutes',seconds,'seconds')
+Badal has been alive for 8395 days 12096804 minutes 725809298 seconds
+>>> print('thanks for using my age calc program')
+thanks for using my age calc program
+>>> 
---
+++ hello.py
@@ -0,0 +1,12 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name = print (input('what is your name?'))
+what is your name?badal
+badal
+>>> age=print (input('what is your age?'))
+what is your age?22
+22
+>>> print('thanks for using my simple program')
+thanks for using my simple program
+>>> 
